3b4e5e08b3aa442af052c611e77ccaad4a447fc5
Kevin See <Kevin@qcinc.org>
2013-07-23 10:13:47 -0700

    Fixed jacknife function
    made sure it returns a dataframe with as many rows as the data,
    including NAs where appropriate

--- R/JackKnifeDepl.R
+++ R/JackKnifeDepl.R
@@ -6,33 +6,32 @@ JackKnifeDepl = function(data, group = NULL, grouped=FALSE)
 {
   names(data) = c('p1', 'p2', 'p3')[1:ncol(data)]
   if(grouped) data$grp = group
+  # set up vectors to capture results
+  results = data.frame(N.hat = rep(NA, nrow(data)), N.hat.SE=NA, p.hat=NA, row.names=rownames(data))
   # which rows having missing data?
-  miss.Dat = sort(unique(which(is.na(data), T)[,1]))
+  miss.data = sort(unique(which(is.na(data), T)[,1]))
   # remove missing data rows
   data = na.omit(data)
-  if(grouped==F){
-    N.est = p.hat = N.est.SE = rep(NA, nrow(data))
-    p.hat = 1 - (sum(rowSums(data[,c('p1', 'p2', 'p3')])) - sum(data$p1)) / (sum(rowSums(data[,c('p1', 'p2', 'p3')])) - sum(data$p3))
-    if(length(miss.Dat)>0){
-      N.est[-miss.Dat] = rowSums(data[,c('p1', 'p2')]) + data$p3/ p.hat
-      N.est.SE[-miss.Dat] = 3*2*data$p3
+  if(grouped==F) {
+    if(length(miss.data)>0) {
+      results$p.hat[-miss.data] = 1 - (sum(rowSums(data[,c('p1', 'p2', 'p3')])) - sum(data$p1)) / (sum(rowSums(data[,c('p1', 'p2', 'p3')])) - sum(data$p3))
+      results$N.hat[-miss.data] = rowSums(data[,c('p1', 'p2')]) + data$p3/ results$p.hat[-miss.data]
+      results$N.hat.SE[-miss.data] = 3*2*data$p3
     }
-    if(length(miss.Dat)==0) {
-      N.est = rowSums(data[,c('p1', 'p2')]) + data$p3/ p.hat
-      N.est.SE = 3*2*data$p3
-    }
-    results = data.frame(N.hat = N.est, N.hat.SE=N.est.SE, p.hat=p.hat)
+    if(length(miss.data)==0) {
+      results$p.hat = 1 - (sum(rowSums(data[,c('p1', 'p2', 'p3')])) - sum(data$p1)) / (sum(rowSums(data[,c('p1', 'p2', 'p3')])) - sum(data$p3))
+      results$N.hat = rowSums(data[,c('p1', 'p2')]) + data$p3/ results$p.hat
+      results$N.hat.SE = 3*2*data$p3
+    }  
   }
-  if(grouped==T){
-    results = data.frame(N.hat = rep(NA, nrow(data)), N.hat.SE=NA, p.hat=NA, row.names=rownames(data))
-    for(group in unique(data$grp))
-    {
-      if(sum(rowSums(data[data$grp==group,c('p1', 'p2', 'p3')]))==0){
+  if(grouped==T) {
+    for(group in unique(data$grp)) {
+      if(sum(rowSums(data[data$grp==group,c('p1', 'p2', 'p3')]))==0) {
         p.hat=NA
         N.est.SE=NA
         N.est = 0
       } 
-      if(sum(rowSums(data[data$grp==group,c('p1', 'p2', 'p3')]))>0){
+      if(sum(rowSums(data[data$grp==group,c('p1', 'p2', 'p3')]))>0) {
         p.hat = 1 - (sum(rowSums(data[data$grp==group,c('p1', 'p2', 'p3')])) - sum(data$p1[data$grp==group])) / (sum(rowSums(data[data$grp==group,c('p1', 'p2', 'p3')])) - sum(data$p3[data$grp==group]))
         N.est = data$p1[data$grp==group] + data$p2[data$grp==group] + data$p3[data$grp==group]/p.hat
         N.est.SE = 3*2*data$p3[data$grp==group]
