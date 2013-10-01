c9bd4c14eac8a79862f349e8a10797eb9abc67de
Kevin See <Kevin@qcinc.org>
2013-10-01 12:16:23 -0700

    include SE estimate for 2-pass depletion

--- R/TwoPassDepl.R
+++ R/TwoPassDepl.R
@@ -2,12 +2,16 @@
 # function to estimate abundance from 2 pass delpletion data #
 ##############################################################
 TwoPassDepl = function(data, rmInvalid=FALSE, p.threshold=0.2){
-  N.hat = data[,1]^2 / (data[,1] - data[,2])
+  y1 = data[,1]
+  y2 = data[,2]
+  N.hat = y1^2 / (y1 - y2)
   # if no fish caught on either pass, make N.hat 0
-  N.hat[which(data[,1]==0 & data[,2]==0)] = 0
-  p.hat = (data[,1] - data[,2]) / data[,1]
+  N.hat[which(y1==0 & y2==0)] = 0
+  p.hat = (y1 - y2) / y1
+  N.hat.SE = sqrt((y1^2 * y2^2 * (y1 + y2)) / (y1 - y2)^4)
   # if the 2nd pass caught more fish than the first, make it invalid
-  N.hat[which(data[,1]<=data[,2])] = p.hat[which(data[,1]<=data[,2])] = NA
-  if(rmInvalid==T) N.hat[p.hat<p.threshold] = NA
-  return(data.frame(N.hat = N.hat, p.hat=p.hat))
+  N.hat[which(y1<=y2)] = N.hat.SE[which(y1<=y2)] = p.hat[which(y1<=y2)] = NA
+  # mark as invalid any estimate with p.hat < p.threshold
+  if(rmInvalid==T) N.hat[p.hat<p.threshold] = N.hat.SE[p.hat<p.threshold] = NA
+  return(data.frame(N.hat = N.hat, N.hat.SE=N.hat.SE, p.hat=p.hat))
 }
