483dc6c2402c368e259f2bc0528084cedb752b9e
Kevin See <Kevin@qcinc.org>
2015-07-01 11:51:48 -0700

    Corrected formula for Seber 3 pass depletion

--- R/GuyBrownDepl.R
+++ R/GuyBrownDepl.R
@@ -3,7 +3,8 @@
 # to estimate abundance from 3 pass delpletion data
 ###############################################################
 GuyBrownDepl = function(data){
-  x = 2 * (data[,1] + data[,2])
+#   x = 2 * (data[,1] + data[,2])
+  x = 2 * data[,1] + data[,2]
   y = rowSums(data, na.rm=T)
   # estimate abundance
   N.hat = (6*x^2 - 3*x*y - y^2 + y*sqrt(y^2 + 6*x*y - 3*x^2)) / (18*(x- y))
@@ -12,6 +13,7 @@ GuyBrownDepl = function(data){
   # calculate standard error of N.hat
   N.hat.SE = sqrt((N.hat*(1-p.hat^3)*p.hat^3) / ((1-p.hat^3)^2 - (3*(1-p.hat))^2*p.hat^2))
   valid = ifelse(N.hat<0 | N.hat==Inf | N.hat==-Inf, F, T)
+  valid = ifelse(data[,3] > data[,1], F, T)
   if(y==0) {
     N.hat = 0
     N.hat.SE=NA
